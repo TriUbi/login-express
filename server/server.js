4c7eac992149e272226da2b89b2d162466d08148
TriUbi <triubilla@gmail.com>
2024-03-26 16:20:34 +0100

    just fixing small things

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const express = require("express")
 
+const userRouter = require("./resources/users/users.router")
 const app = express()
 
-app.get("/users", (req, res)=> {
-    
-})
+app.use("/apiusers", userRouter); 
 
 app.listen(3001, () => console.log("Server is up and runninggg..."))
